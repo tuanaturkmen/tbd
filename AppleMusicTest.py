1dd0cf2d94ecf2580ab3144b2e1b12910d52581c
Debug <onuur94@hotmail.com>
2024-05-30 13:25:26 +0300

    Update AppleMusicTest.py
    Telif hakkı

--- AppleMusicTest.py
+++ AppleMusicTest.py
@@ -1,3 +1,7 @@
+# Copyright © 2024 Tüm Hakları Saklıdır.
+# Developer: Onur KÖSE
+# Nick Name: MrRadar
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
